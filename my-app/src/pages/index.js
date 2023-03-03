039382e5bed779f306653fc4252ff6a6be257346
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2023-03-03 19:34:26 +0530

    Almost Frontend

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,63 +1,91 @@
-import styles from '@/styles/Home.module.css'
+import { useState } from 'react';
 
 
 export default function Home() {
+
+  const [form, setForm] = useState({name: "", roll: "", col: ""});
+
+  const handleChange = (event)=>{
+    let name = event.target.name;
+    let value = event.target.value;
+
+    setForm({ ...form, [name]: value });
+  }
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log("Submit")
+  }
+
   return (
     <>
-     <div className="items-center">
-       <h1>Registration Form</h1>
-       <div className="w-full max-w-xs">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="username"
-      >
-        Username
-      </label>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="username"
-        type="text"
-        placeholder="Username"
-      />
-    </div>
-    <div className="mb-6">
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="password"
-      >
-        Password
-      </label>
-      <input
-        className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        id="password"
-        type="password"
-        placeholder="******************"
-      />
-      <p className="text-red-500 text-xs italic">Please choose a password.</p>
-    </div>
-    <div className="flex items-center justify-between">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        type="button"
-      >
-        Sign In
-      </button>
-      <a
-        className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-        href="#"
-      >
-        Forgot Password?
-      </a>
-    </div>
-  </form>
-  <p className="text-center text-gray-500 text-xs">
-    ©2020 Acme Corp. All rights reserved.
-  </p>
-</div>
+      <h1 className="my-8 text-4xl text-center">Registration Form</h1>
+      <div className="flex justify-center">
+        <div className="w-full max-w-xs">
+          <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
+              >
+                Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={form.name}
+                onChange={handleChange}
+              />
+            </div>
 
-     </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="roll"
+              >
+                Roll Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="Roll Number"
+                name="roll"
+                value={form.roll}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="colname"
+              >
+                College Name
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                placeholder="College Name"
+                name="col"
+                value={form.col}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="flex justify-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
